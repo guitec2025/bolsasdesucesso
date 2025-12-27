1bcbaa9356946e567f6744d8cfb91e435708a980
Guilherme MA <Guitec2016@gmail.com>
2025-12-27 21:47:19 +0000

    o botão da caixa de oferta deve levar para esse link por favcr https://p

--- src/components/landing/pricing.tsx
+++ src/components/landing/pricing.tsx
@@ -69,12 +69,15 @@ export function Pricing() {
                 </div>
             </div>
             <Button
+              asChild
               size="lg"
               className='w-full h-14 text-lg font-medium text-white'
               style={{ backgroundColor: '#478552' }}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sparkles mr-2"><path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z"/><path d="M5 3v4"/><path d="M19 17v4"/><path d="M3 5h4"/><path d="M17 19h4"/></svg>
-              QUERO ACESSO VITALÍCIO
+              <a href="https://pay.cakto.com.br/378bsy3_701080">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sparkles mr-2"><path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z"/><path d="M5 3v4"/><path d="M19 17v4"/><path d="M3 5h4"/><path d="M17 19h4"/></svg>
+                QUERO ACESSO VITALÍCIO
+              </a>
             </Button>
           </CardContent>
           <CardFooter className="flex-col gap-2 px-6 pb-6">
