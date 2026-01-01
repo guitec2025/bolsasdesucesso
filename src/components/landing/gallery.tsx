c22f9320cfcc40cbda3e24d699ed3e69dc5da40e
Guilherme MA <Guitec2016@gmail.com>
2026-01-01 04:23:06 +0000

    na visualização de computador, deixe normal essas imagens..

--- src/components/landing/gallery.tsx
+++ src/components/landing/gallery.tsx
@@ -68,7 +68,7 @@ export function Gallery() {
       )
     }
     return (
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 sm:gap-6 mt-8">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 sm:gap-6 mt-8">
         {images.map((image) => (
             <div 
               key={image.id} 
