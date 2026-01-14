e288413fb7a0cd50487a300aaf46e012fd87aef0
Guilherme MA <Guitec2016@gmail.com>
2026-01-14 12:48:59 +0000

    por favor coloque isso no meu codigo: fbq('init', '836192886056120', {

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '836192886056120');
+            fbq('init', '836192886056120', {});
             fbq('track', 'PageView');
           `}
         </Script>
