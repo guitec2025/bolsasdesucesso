844beccdba4d34272a9aa691d557405df60a213c
Guilherme MA <Guitec2016@gmail.com>
2026-02-02 01:29:55 +0000

    o botão "QUERO ACESSO VITALÍCIO" da caixa de oferta deve levar para esse

--- src/components/landing/pricing.tsx
+++ src/components/landing/pricing.tsx
@@ -74,7 +74,7 @@ export function Pricing() {
               className='w-full h-14 text-lg font-medium text-white'
               style={{ backgroundColor: '#478552' }}
             >
-              <a href="https://checkout.sucessoderotina.com/VCCL1O8SCPBX">
+              <a href="https://checkout.bolsasdesucesso.com/VCCL1O8SCPBX">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-sparkles mr-2"><path d="m12 3-1.912 5.813a2 2 0 0 1-1.275 1.275L3 12l5.813 1.912a2 2 0 0 1 1.275 1.275L12 21l1.912-5.813a2 2 0 0 1 1.275-1.275L21 12l-5.813-1.912a2 2 0 0 1-1.275-1.275L12 3Z"/><path d="M5 3v4"/><path d="M19 17v4"/><path d="M3 5h4"/><path d="M17 19h4"/></svg>
                 QUERO ACESSO VITALÍCIO
               </a>
