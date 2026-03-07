8fcab63f747aec7ebb610b65b2b82b614e884e93
Guilherme MA <Guitec2016@gmail.com>
2026-03-07 20:27:00 +0000

    por favor substitua meu pixel da meta por esses scripts aqui que são meu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,25 +53,24 @@ export default function RootLayout({
         {/* End Google Tag Manager (noscript) */}
         {children}
         <Toaster />
-        <Script id="meta-pixel" strategy="afterInteractive">
+        
+        {/* Utmify Scripts */}
+        <Script
+          src="https://cdn.utmify.com.br/scripts/utms/latest.js"
+          data-utmify-prevent-xcod-sck
+          data-utmify-prevent-subids
+          strategy="afterInteractive"
+        />
+        <Script id="utmify-pixel" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '836192886056120', { em: 'email@email.com', ph: '1234567890' });
-            fbq('track', 'PageView');
+            window.pixelId = "69ac892bcb4572594d7a7f5e";
+            var a = document.createElement("script");
+            a.setAttribute("async", "");
+            a.setAttribute("defer", "");
+            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+            document.head.appendChild(a);
           `}
         </Script>
-        <noscript>
-          <img height="1" width="1" style={{display: 'none'}}
-            src="https://www.facebook.com/tr?id=836192886056120&ev=PageView&noscript=1"
-          />
-        </noscript>
       </body>
     </html>
   );
