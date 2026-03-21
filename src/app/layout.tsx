9d3001f38ce4407f0c6ff5de49b35748b5a140fc
Guilherme Silva <Guitec2016@gmail.com>
2026-03-21 03:36:54 +0000

    seguinte substitua o pixel google tag por esses: 1. Cole este código o

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,14 +41,14 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NFMT9GR4');
+            })(window,document,'script','dataLayer','GTM-5TX8TDBS');
           `}
         </Script>
         {/* End Google Tag Manager */}
       </head>
       <body className={cn("font-body antialiased", alegreya.variable, belleza.variable)}>
         {/* Google Tag Manager (noscript) */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NFMT9GR4"
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5TX8TDBS"
         height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
         {/* End Google Tag Manager (noscript) */}
         {children}
