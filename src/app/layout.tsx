ba2def11a486de2160954e3c7adda57b7e44a6ed
Guilherme Silva <Guitec2016@gmail.com>
2026-03-21 00:47:36 +0000

    seguinte estou configurando meu google tag manager. coloque no meu site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,14 +41,14 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5C63QDLL');
+            })(window,document,'script','dataLayer','GTM-NFMT9GR4');
           `}
         </Script>
         {/* End Google Tag Manager */}
       </head>
       <body className={cn("font-body antialiased", alegreya.variable, belleza.variable)}>
         {/* Google Tag Manager (noscript) */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5C63QDLL"
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NFMT9GR4"
         height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
         {/* End Google Tag Manager (noscript) */}
         {children}
