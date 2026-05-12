ed1b9f8661934c53d7fc32a088fbc482216884a4
Guilherme Silva <Guitec2016@gmail.com>
2026-05-12 23:20:32 +0000

    implemente na pagina o pixel do clarity por favor: <script type="text/ja

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,18 @@ export default function RootLayout({
           `}
         </Script>
         {/* End Google Tag Manager */}
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wq6jprb9q0");
+          `}
+        </Script>
+        {/* End Microsoft Clarity */}
       </head>
       <body className={cn("font-body antialiased", alegreya.variable, belleza.variable)}>
         {/* Google Tag Manager (noscript) */}
