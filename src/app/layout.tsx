fcf6541d74aa21f7a87f7c7ccf328fcbe96cfaff
zuppi2025 <zuppis18@gmail.com>
2026-07-26 00:40:08 -0300

    Add Google Tag Manager container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,17 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5TX8TDBS');
+          `}
+        </Script>
+
         {/* Stape GTM Loader - Carregado uma única vez com ID estável */}
         <Script 
           id="gtm-loader" 
@@ -75,6 +86,15 @@ export default function RootLayout({
         />
       </head>
       <body className={cn("font-body antialiased", alegreya.variable, beleza.variable)}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5TX8TDBS"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
         <Toaster />
       </body>
